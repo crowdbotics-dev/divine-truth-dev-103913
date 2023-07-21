d32764c47937de369c559b89824e86aaa8fa2824
Crowdbotics <engineering@crowdbotics.com>
2023-07-21 13:20:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "divine-truth-dev-103913",
+  url: "https://divine-truth-dev-103913.botics.co",
+  api: "https://divine-truth-dev-103913.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
